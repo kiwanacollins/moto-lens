c07cec8453f86eb889b6b11e704d6a80c9e79df6
kiwanacollins <collinzcalson@gmail.com>
2026-01-28 09:27:58 +0300

    fix: update revision number for index.html in service worker

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4mep1pba9o"
+    "revision": "0.nia4ctqln9"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
